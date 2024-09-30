b6199df69c4429a202d1470fa369d1380948a16d
Jennifer Rush <jennifer.rush2@gmail.com>
2024-09-30 13:17:43 -0600

    creates order tickets by age method and show page setup for employees

--- spec/features/employees/show_spec.rb
+++ spec/features/employees/show_spec.rb
@@ -15,8 +15,16 @@ RSpec.describe "Emplee show page" do
         @it.employees.create!(name:"Christina Aguilera", level: 2)
         @it.employees.create!(name:"Bob Jones", level: 4)
 
-        @communications.employees.create!(name: "James Bond", level: 125)
+        @employee_3 = @communications.employees.create!(name: "James Bond", level: 125)
         @employee_4 = @communications.employees.create!(name: "Kamala Harris", level: 236)
+
+        @ticket_1 = @employee_4.tickets.create!(subject:"printers broken", age: 5)
+        @ticket_2 = @employee_4.tickets.create!(subject:"hallway vandalized", age: 2)
+        @ticket_3 = @employee_4.tickets.create!(subject:"chair broken", age: 3)
+
+        @ticket_4 = @employee_3.tickets.create!(subject:"blood in hallway", age: 6)
+
+
     end
 
     it "shows the employee's name and department" do
@@ -24,10 +32,16 @@ RSpec.describe "Emplee show page" do
 
         expect(page).to have_content("Kamala Harris")
         expect(page).to have_content("Communications")
+        expect(page).to_not have_content("James Bond")
 
     end
 
-    xit "shows a list of all the employee's tickets oldest to newest" do
+    it "shows a list of all the employee's tickets oldest to newest" do
+        visit "/employees/#{@employee_4.id}"
+        
+        expect("printers broken").to appear_before("chair broken")
+        expect("chair broken").to appear_before("hallway vandalized")
+    
     end
 
     xit "shows the oldest ticket listed separately" do
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -6,4 +6,27 @@ RSpec.describe Employee, type: :model do
     it {should have_many :employee_tickets}
     it {should have_many(:tickets).through(:employee_tickets)}
   end
+
+  describe "instance methods" do
+    before :each do
+      @it = Department.create!(name:"IT", floor:"Basement")
+        @communications = Department.create!(name:"Communications", floor:"Ground")
+
+        @it.employees.create!(name:"Christina Aguilera", level: 2)
+        @it.employees.create!(name:"Bob Jones", level: 4)
+
+        @employee_3 = @communications.employees.create!(name: "James Bond", level: 125)
+        @employee_4 = @communications.employees.create!(name: "Kamala Harris", level: 236)
+
+        @ticket_1 = @employee_4.tickets.create!(subject:"printers broken", age: 5)
+        @ticket_2 = @employee_4.tickets.create!(subject:"hallway vandalized", age: 2)
+        @ticket_3 = @employee_4.tickets.create!(subject:"chair broken", age: 3)
+
+        @ticket_4 = @employee_3.tickets.create!(subject:"blood in hallway", age: 6)
+    end
+
+    it "orders an employees tickets oldest to newest" do
+      expect(@employee_4.order_tickets_by_oldest).to eq([@ticket_1, @ticket_3, @ticket_2])
+    end
+  end
 end
